2bd5ecd23469ff70d9dce55d34302ff85a1a4567
caiodrigo <111082445+caiodrigo@users.noreply.github.com>
2022-10-06 16:45:45 -0300

    Add files via upload

---
+++ Pera uva maça/src/main/java/br/edu/uerr/loja/uva.java
@@ -0,0 +1,13 @@
+package br.edu.uerr.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bananas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bananas1Application.class, args);
+	}
+
+}
